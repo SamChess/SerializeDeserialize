d0e7fcc79bd92a292d95246d2759460b28eb77fe
unknown <fcbsammymulwa@gmail.com>
2020-11-01 20:27:50 +0300

    deserialize and serialize C# code

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
+namespace Lab1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var student = new Student();
+            student.FirstName = "Samuel";
+            student.LastName = "Mulwa";
+            student.Age = 21;
+
+            //Serialize
+            System.Xml.Serialization.XmlSerializer x = new System.Xml.Serialization.XmlSerializer(student.GetType());
+            x.Serialize(Console.Out, student);
+            Console.WriteLine();
+            Console.ReadLine();
+GCNotificationStatus init
+            //Deserialize an xml file    
+            XmlSerializer deserializer = new XmlSerializer(typeof(Student));
+            System.IO.TextReader reader = new System.IO.StreamReader(@"D:\lab1\src\Lab1\deserialize.xml");
+            object obj = deserializer.Deserialize(reader);
+            Student XmlData = (Student)obj;
+            Console.WriteLine(XmlData);
+            reader.Close();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,12 @@
+namespace Lab1{
+
+    // class that will be serialized
+
+    public class Student{
+
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public int Age { get; set;}
+
+    }
+}
